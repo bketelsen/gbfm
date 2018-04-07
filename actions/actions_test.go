bfd31cc866c0842832fabcc7e7636f0022f984de
Aaron Schlesinger <arschles@gmail.com>
2018-04-06 17:31:49 -0700

    tests for the new packages

--- actions/actions_test.go
+++ actions/actions_test.go
@@ -14,3 +14,13 @@ func Test_ActionSuite(t *testing.T) {
 	as := &ActionSuite{suite.NewAction(App())}
 	suite.Run(t, as)
 }
+
+func Test_GBFM_ActionSuite(t *testing.T) {
+	as := &gbfm.ActionSuite{suite.NewAction(App())}
+	suite.Run(t, as)
+}
+
+func Test_Snacks_ActionSuite(t *testing.T) {
+	as := &snacks.ActionSuite{suite.NewAction(App())}
+	suite.Run(t, as)
+}
