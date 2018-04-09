8f3c8d8ee881224f4301f1052b7ab757fa8db478
Aaron Schlesinger <arschles@gmail.com>
2018-04-09 15:13:11 -0700

    getting the snack detail page right

--- actions/snacks/snack.go
+++ actions/snacks/snack.go
@@ -1,13 +1,17 @@
 package snacks
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gobuffalo/buffalo"
 )
 
 func snackHandler(c buffalo.Context) error {
-	slug := c.Param("slug")
+	slug := c.Param("snack_slug")
+	if slug == "" {
+		return c.Error(http.StatusBadRequest, errors.New("no snack slug found"))
+	}
 	c.Set("slug", slug)
-	return c.Render(http.StatusOK, r.HTML("snack.html"))
+	return c.Render(http.StatusOK, r.HTML("snacks/snack.html"))
 }
