e84371812512f14519652984203fe9696b8ec7e3
Aaron Schlesinger <arschles@gmail.com>
2018-04-13 14:54:15 -0400

    fixing migration for guide

--- models/guide.go
+++ models/guide.go
@@ -4,14 +4,14 @@ package models
 type Guide struct {
 	coreModel
 	slugger
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Thumbnail   string   `json:"thumbnail"`
-	EmbedCode   string   `json:"embed_code"`
-	Body        string   `json:"body"`
-	Pro         bool     `json:"pro"`
-	Topics      []Topic  `json:"topics" db:"topics" many_to_many:"topics_guides"`
-	Authors     []Author `json:"authors" db:"authors" many_to_many:"authors_guides"`
+	Title        string   `json:"title" db:"title"`
+	Description  string   `json:"description" db:"description"`
+	ThumbnailURL string   `json:"thumbnail_url" db:"thumbnail_url"`
+	EmbedCode    string   `json:"embed_code" db:"embed_code"`
+	Body         string   `json:"body" db:"body"`
+	Pro          bool     `json:"pro" db:"pro"`
+	Topics       []Topic  `json:"topics" db:"topics" many_to_many:"guides_topics"`
+	Authors      []Author `json:"authors" db:"authors" many_to_many:"guides_authors"`
 }
 
 func init() {
