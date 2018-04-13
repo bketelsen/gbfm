cd94b91f6f374ae9c0020d2ce9aaee17ea747f93
Aaron Schlesinger <arschles@gmail.com>
2018-04-13 14:46:08 -0400

    Thumbnail => ThumbnailURL

--- models/episode.go
+++ models/episode.go
@@ -5,15 +5,15 @@ type Episode struct {
 	coreModel
 	slugger
 
-	Title       string   `json:"title" db:"title"`
-	Description string   `json:"description" db:"description"`
-	Thumbnail   string   `json:"thumbnail_url" db:"thumbnail_url"`
-	EmbedCode   string   `json:"embed_code" db:"embed_code"`
-	Body        string   `json:"body" db:"body"`
-	Pro         bool     `json:"pro" db:"pro"`
-	Repo        string   `json:"repo" db:"repo"`
-	Topics      []Topic  `json:"topics" db:"topics" many_to_many:"episodes_topics"`
-	Authors     []Author `json:"authors" db:"authors" many_to_many:"episodes_authors"`
+	Title        string   `json:"title" db:"title"`
+	Description  string   `json:"description" db:"description"`
+	ThumbnailURL string   `json:"thumbnail_url" db:"thumbnail_url"`
+	EmbedCode    string   `json:"embed_code" db:"embed_code"`
+	Body         string   `json:"body" db:"body"`
+	Pro          bool     `json:"pro" db:"pro"`
+	Repo         string   `json:"repo" db:"repo"`
+	Topics       []Topic  `json:"topics" db:"topics" many_to_many:"episodes_topics"`
+	Authors      []Author `json:"authors" db:"authors" many_to_many:"episodes_authors"`
 }
 
 func init() {
