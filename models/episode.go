49ae96ff0b1ee50ba2f781ebe88b452612d53e3c
Aaron Schlesinger <arschles@gmail.com>
2018-04-13 14:38:42 -0400

    fixing the episode migration
    also getting many-to-many right

--- models/episode.go
+++ models/episode.go
@@ -7,13 +7,13 @@ type Episode struct {
 
 	Title       string   `json:"title" db:"title"`
 	Description string   `json:"description" db:"description"`
-	Thumbnail   string   `json:"thumbnail" db:"thumbnail"`
+	Thumbnail   string   `json:"thumbnail_url" db:"thumbnail_url"`
 	EmbedCode   string   `json:"embed_code" db:"embed_code"`
 	Body        string   `json:"body" db:"body"`
 	Pro         bool     `json:"pro" db:"pro"`
 	Repo        string   `json:"repo" db:"repo"`
-	Topics      []Topic  `json:"topics" db:"topics" has_many:"topics"`
-	Authors     []Author `json:"authors" db:"authors" has_many:"authors"`
+	Topics      []Topic  `json:"topics" db:"topics" many_to_many:"episodes_topics"`
+	Authors     []Author `json:"authors" db:"authors" many_to_many:"episodes_authors"`
 }
 
 func init() {
