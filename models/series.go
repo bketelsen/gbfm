e85719c2b811d6fa799abc660efe4bf335897a0e
Aaron Schlesinger <arschles@gmail.com>
2018-04-13 14:56:52 -0400

    fixing the series migration

--- models/series.go
+++ models/series.go
@@ -3,13 +3,13 @@ package models
 type Series struct {
 	coreModel
 	slugger
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Thumbnail   string   `json:"thumbnail"`
-	Body        string   `json:"body"`
-	Pro         bool     `json:"pro"`
-	Topics      []Topic  `json:"topics" db:"topics" many_to_many:"topics_series"`
-	Authors     []Author `json:"authors" db:"authors" many_to_many:"authors_series"`
+	Title        string   `json:"title" db:"title"`
+	Description  string   `json:"description" db:"description"`
+	ThumbnailURL string   `json:"thumbnail_url" db:"thumbnail_url`
+	Body         string   `json:"body" db:"body"`
+	Pro          bool     `json:"pro" db:"pro"`
+	Topics       []Topic  `json:"topics" db:"topics" many_to_many:"series_topics"`
+	Authors      []Author `json:"authors" db:"authors" many_to_many:"series_authors"`
 }
 
 func init() {
