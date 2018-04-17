4dbe667183162e24f154fd020242f4e773d8687c
Aaron Schlesinger <aaschles@microsoft.com>
2018-04-17 15:20:00 -0700

    Making episodes test do a deep-equal test on authors and topics

--- models/episode_test.go
+++ models/episode_test.go
@@ -16,6 +16,16 @@ func (ms *ModelSuite) TestRoundTripEpisode() {
 	retEp := &Episode{}
 	r.NoError(db.Eager().Find(retEp, ep.ID))
 	r.Equal(ep.ID.String(), retEp.ID.String())
-	r.Equal(ep.Authors, retEp.Authors)
-	r.Equal(ep.Topics, retEp.Topics)
+
+	// verify that authors were saved and queried properly
+	r.Equal(len(ep.Authors), len(retEp.Authors))
+	for i, expectedAuthor := range ep.Authors {
+		r.Equal(expectedAuthor.ID.String(), ep.Authors[i].ID.String())
+	}
+
+	// verify that topics were saved and queries properly
+	r.Equal(len(ep.Topics), len(retEp.Topics))
+	for i, expectedTopic := range ep.Topics {
+		r.Equal(expectedTopic.ID.String(), ep.Topics[i].ID.String())
+	}
 }
