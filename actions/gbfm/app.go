c81c8b5899c8bb8d5c735c253602d59fba123683
Aaron Schlesinger <arschles@gmail.com>
2018-04-17 08:59:50 -0700

    Moving the gbfm app completely into the actions/gbfm dir (#9)

--- actions/gbfm/app.go
+++ actions/gbfm/app.go
@@ -1,31 +1,31 @@
-package actions
+package gbfm
 
 import (
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/middleware"
 	"github.com/gobuffalo/buffalo/middleware/ssl"
 	"github.com/gophersnacks/gbfm/pkg/render"
+	"github.com/gophersnacks/gbfm/pkg/web"
 	"github.com/unrolled/secure"
 
 	"github.com/gobuffalo/buffalo/middleware/csrf"
-	"github.com/gophersnacks/gbfm/actions/gbfm"
 	"github.com/gophersnacks/gbfm/models"
 )
 
-// GBFMApp is where all routes and middleware for gobeyond5minutes.com are defined
-func GBFMApp() *buffalo.App {
+// App is where all routes and middleware for gobeyond5minutes.com are defined
+func App() *buffalo.App {
 	app := buffalo.New(buffalo.Options{
 		Addr:        "0.0.0.0:3001",
-		Env:         ENV,
+		Env:         web.ENV,
 		SessionName: "_gbfm_session",
 	})
 	// Automatically redirect to SSL
 	app.Use(ssl.ForceSSL(secure.Options{
-		SSLRedirect:     ENV == "production",
+		SSLRedirect:     web.ENV == "production",
 		SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
 	}))
 
-	if ENV == "development" {
+	if web.ENV == "development" {
 		app.Use(middleware.ParameterLogger)
 	}
 
@@ -39,23 +39,12 @@ func GBFMApp() *buffalo.App {
 	app.Use(middleware.PopTransaction(models.DB))
 
 	// Setup and use translations:
-	app.Use(translator.Middleware())
-	gbfm.AddRoutes(app)
-
-	admin := app.Group("/admin")
-	admin.Use(SetCurrentUser)
-	admin.Use(AdminAuthorize)
-	admin.GET("/", AdminHandler)
-
-	app.Use(SetCurrentUser)
-	app.GET("/users/new", UsersNew)
-	app.POST("/users", UsersCreate)
-	app.GET("/signin", AuthNew)
-	app.POST("/signin", AuthCreate)
-	app.POST("/token", AuthToken)
-	app.DELETE("/signout", AuthDestroy)
-	app.GET("/authors", AuthorList)
-	app.GET("/authors/{name}", AuthorShow)
+	app.Use(web.Translator.Middleware())
+	app.GET("/", homeHandler)
+	app.Resource("/series", SeriesResource{})
+	app.Resource("/guides", GuidesResource{})
+	app.GET("/episodes", EpisodeList)
+	app.GET("/episodes/{name}", EpisodeShow)
 	app.ServeFiles("/", render.AssetsBox) // serve files from the public directory
 
 	return app
---
+++ actions/gbfm/app_test.go
@@ -0,0 +1,16 @@
+package gbfm
+
+import (
+	"testing"
+
+	"github.com/gobuffalo/suite"
+)
+
+type ActionSuite struct {
+	*suite.Action
+}
+
+func Test_ActionSuite(t *testing.T) {
+	as := &ActionSuite{suite.NewAction(App())}
+	suite.Run(t, as)
+}
