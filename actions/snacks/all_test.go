93437df62663f7b9094bfd5293013e8897d2ac90
Aaron Schlesinger <aaschles@microsoft.com>
2018-04-19 09:41:03 -0700

    Adding more tests to the actions
    Ref https://github.com/gophersnacks/gbfm/issues/12

--- actions/snacks/all_test.go
+++ actions/snacks/all_test.go
@@ -1,9 +1,19 @@
 package snacks
 
 import (
+	"testing"
+
 	"github.com/gobuffalo/suite"
+	"github.com/technosophos/moniker"
 )
+
+var namer = moniker.New()
 
 type ActionSuite struct {
 	*suite.Action
+}
+
+func TestSnacksActionSuite(t *testing.T) {
+	as := &ActionSuite{suite.NewAction(App())}
+	suite.Run(t, as)
 }
