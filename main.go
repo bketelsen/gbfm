9318d80208ad5385ce95e56f1831256dd1ae3ba2
Brian Ketelsen <bketelsen@gmail.com>
2018-04-23 06:00:34 -0400

    closer and closer

--- main.go
+++ main.go
@@ -5,24 +5,23 @@ import (
 	"log"
 
 	"github.com/gophersnacks/gbfm/actions/admin"
+	"github.com/gophersnacks/gbfm/actions/gbfm"
 	"github.com/gophersnacks/gbfm/actions/snacks"
 )
 
 func main() {
 
 	snacksApp := snacks.App()
-	//gbfmApp := gbfm.App()
+	gbfmApp := gbfm.App()
 	//	contentApp := content.App()
 
 	errCh := make(chan error)
-	/*
-		go func() {
-			if err := gbfmApp.Serve(); err != nil {
-				log.Printf("ERROR: gbfm app crashed")
-				errCh <- err
-			}
-		}()
-	*/
+	go func() {
+		if err := gbfmApp.Serve(); err != nil {
+			log.Printf("ERROR: gbfm app crashed")
+			errCh <- err
+		}
+	}()
 	go func() {
 		if err := snacksApp.Serve(); err != nil {
 			log.Printf("ERROR: snacks app crashed")
