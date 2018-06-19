2c47ee31233f3d11fc43fb756938ce546f710dc5
Aaron Schlesinger <aaschles@microsoft.com>
2018-06-19 16:13:44 -0700

    cleaning up

--- actions/admin/gh_provider.go
+++ actions/admin/gh_provider.go
@@ -53,75 +53,30 @@ func (ap ghProvider) ConfigAuth(a *auth.Auth) {
 
 // /login/github
 func (ap ghProvider) Login(c *auth.Context) {
-	// fmt.Println("auth0 login")
-	// now := time.Now()
-	// c.Auth.Login(c.Writer, c.Request, &claims.Claims{
-	// 	Provider:    ap.GetName(),
-	// 	UserID:      "aaron",
-	// 	LastLoginAt: &now,
-	// })
-	// fmt.Fprint(c.Writer, "logged in!")
-	fmt.Println("login!")
-	gothic.BeginAuthHandler(c.Writer, c.Request)
 }
 
 // /logout/github
 func (ap ghProvider) Logout(c *auth.Context) {
-	fmt.Println("logout!")
-	// w, r := c.Writer, c.Request
-	// gothic.Logout(w, r)
-	// w.Header().Set("Location", "/")
-	// w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func (ap ghProvider) Register(c *auth.Context) {
-	fmt.Println("register!")
-	w, r := c.Writer, c.Request
-
-	// try to see if the person is logged in already
-	gothUser, err := gothic.CompleteUserAuth(w, r)
-	if err == nil {
-		c.SessionStorerInterface.Update(w, r, &claims.Claims{
-			Provider: ap.GetName(),
-			UserID:   gothUser.UserID,
-		})
-		return
-	}
-
-	// otherwise start auth
-	gothic.BeginAuthHandler(w, r)
-	return
-
 }
 
 func (ap ghProvider) Callback(c *auth.Context) {
-	fmt.Println("callback!")
-	w, r := c.Writer, c.Request
-	gothUser, err := gothic.CompleteUserAuth(w, r)
-	if err != nil {
-		fmt.Fprintln(w, "auth failed!")
-		return
-	}
-	c.SessionStorer.Update(w, r, &claims.Claims{
-		Provider: ap.GetName(),
-		UserID:   gothUser.UserID,
-	})
-	return
 }
 
 func (ap ghProvider) ServeHTTP(c *auth.Context) {
-	fmt.Println("serve http!")
 	router := mux.NewRouter()
 
 	// login start
 	router.HandleFunc(loginPath, func(w http.ResponseWriter, r *http.Request) {
-		// if _, err := gothic.CompleteUserAuth(w, r); err == nil {
-		// 	// t, _ := template.New("foo").Parse(userTemplate)
-		// 	// t.Execute(res, gothUser)
-		// } else {
-		r.URL.Query().Set("provider", "github")
-		gothic.BeginAuthHandler(w, r)
-		// }
+		u, err := gothic.GetAuthURL(c.Writer, c.Request)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(c.Writer, err)
+			return
+		}
+		http.Redirect(c.Writer, c.Request, u, http.StatusTemporaryRedirect)
 	})
 
 	// GH callback
@@ -131,14 +86,12 @@ func (ap ghProvider) ServeHTTP(c *auth.Context) {
 			fmt.Fprintln(w, r)
 			return
 		}
-		// t, _ := template.New("foo").Parse(userTemplate)
-		// t.Execute(res, user)
 	}).Methods("GET")
 
 	// logout
 	router.HandleFunc(logoutPath, func(w http.ResponseWriter, r *http.Request) {
 		gothic.Logout(w, r)
-		w.Header().Set("Location", "/")
+		w.Header().Set("Location", "/admin")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	}).Methods("GET")
 
